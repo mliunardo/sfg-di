eba1dab22a4fee3758a6e96f9e23d4749314d689
mliunardo <mliunardo@gmail.com>
2020-03-21 12:21:36 +0800

    Section 3: 34. Dependency Injection without Spring

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,25 @@
+package guru.springframework.sfgdi.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+
+class ConstructorInjectedControllerTest {
+
+	ConstructorInjectedController controller;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		controller = new ConstructorInjectedController(new GreetingServiceImpl());
+		
+	}
+
+	@Test
+	void testGetGreeting() {
+		System.out.println(controller.getGreeting());
+	}
+
+}
