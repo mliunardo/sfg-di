fc150acec958328a07e06f09450eaaa9c6c2cbf8
mliunardo <mliunardo@gmail.com>
2020-03-23 14:43:27 +0800

    Section 3: 35. Dependency Injection using Spring Framework

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import guru.springframework.sfgdi.services.GreetingService;
@@ -8,10 +9,10 @@ import guru.springframework.sfgdi.services.GreetingService;
 @Controller
 public class ConstructorInjectedController {
 	
-	@Autowired
 	private final GreetingService greetingService;		// Spring 5: Constructor DI - @Autowired is Optional
-
-	public ConstructorInjectedController(GreetingService greetingService) {
+	
+	@Autowired
+	public ConstructorInjectedController(@Qualifier("constructorGreetingService") GreetingService greetingService) {
 		super();
 		this.greetingService = greetingService;
 	}
--- src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 import guru.springframework.sfgdi.services.GreetingService;
@@ -8,9 +9,10 @@ import guru.springframework.sfgdi.services.GreetingService;
 @Controller
 public class SetterInjectedController {
 
-	@Autowired
 	private GreetingService greetingService;
 
+	@Qualifier("setterInjectedGreetingService")
+	@Autowired
 	public void setGreetingService(GreetingService greetingService) {
 		this.greetingService = greetingService;
 	}
